9f238a1eb5bcc1ef7702ebf0fb07e5011c4d339a
Jeremy Chone <jeremy.chone@gmail.com>
2017-09-25 00:31:05 -0700

    . updated comments in mock/_setup.js, and added the rewritemock way

--- server/_test/mock/_setup.js
+++ server/_test/mock/_setup.js
@@ -1,24 +1,28 @@
 console.log("mock/_setup.js to alias all of the mock modules");
 
-
-// --------- Mockery Way --------- //
+// --------- mockery way --------- //
 const mockery = require("mockery");
 mockery.enable();
 mockery.registerAllowable("first");
 mockery.warnOnUnregistered(false);
 
-// right now, we do by hand
-// TOTO: Need to make baseUrl work so that we have only one path per module. 
-//       With tsc (easy, tsconfig), node (probably with module-alias), 
-//       VSCode (not sure how, because it does not seem to follow tsconfig)
+
 const mockBar = require("./mock-bar");
-mockery.registerMock("bar", mockBar);
-// mockery.registerMock("./bar", mockBar); // if we want to catch this as well.
-// --------- /Mockery Way --------- //
+mockery.registerMock("bar", mockBar); // Note: this plays nice with ts/ts-node baseUrl (because it does not try to resolve 'base', just swap it)
+// mockery.registerMock("./bar", mockBar); // Note: would need to register all used path to the same module
+// --------- /mockery way --------- //
 
 
-// --------- ModuleAlias --------- //
+// --------- module-alias way --------- //
 // NOTE: This is an alternative way. Mockery gives more granularity, but module-alias might be required anytway for the baseUrl to work.
 // const moduleAlias = require('module-alias');
 // moduleAlias.addAlias('../bar', __dirname + '/mock-bar');
-// --------- /ModuleAlias --------- //
+// --------- /module-alias way --------- //
+
+// --------- rewiremock way  --------- //
+// Note: Not working with baseUrl, rewritemock tries to find the 'bar', but because it not exec by ts-node, the mapping is not done.
+// const rewire = require("rewiremock").default;
+// rewire('bar').by(__dirname + '/mock-bar'); // Note: this does NOT play nice with ts/ts-node baseUrl (module 'bar' not found)
+// rewire('../bar').by(__dirname + '/mock-bar'); // <-- that work fine, but require relative addressing
+//rewire.enable();
+// --------- /rewiremock way  --------- //
