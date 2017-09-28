988b0c26c7daceb831e233f58463b2a76831dae6
Jeremy Chone <jeremy.chone@gmail.com>
2017-09-28 15:46:38 -0700

    . remove unused test

--- server/src/_test/mock/_setup.js
+++ server/src/_test/mock/_setup.js
@@ -2,24 +2,23 @@ const alias = require('module-alias');
 
 console.log('mock/_setup.js start');
 
-// we set the common alias and the server/src as path, to mirror server/tsconfig.json (when running)
+
+// --------- Module Alias --------- //
+///// We set the module alias to match the server/tsconfig.json even in test files
 const projectDir = __dirname + '/../../../../';
 alias.addAlias('common', projectDir + 'common');
 alias.addPath(projectDir + 'server/src');
+// --------- /Module Alias --------- //
 
-// --------- mockery way --------- //
+// --------- mockery --------- //
 const mockery = require('mockery');
 mockery.enable();
 mockery.registerAllowable('first');
 mockery.warnOnUnregistered(false);
 
 
-const mockBar = require('./mock-bar');
-mockery.registerMock('bar', mockBar); // Note: this plays nice with ts/ts-node baseUrl (because it does not try to resolve 'base', just swap it)
-
 const mockConf = require('./conf');
 mockery.registerMock('conf', mockConf);
+// --------- /mockery --------- //
 
 console.log('mock/_setup.js end');
-// mockery.registerMock("./bar", mockBar); // Note: would need to register all used path to the same module
-// --------- /mockery way --------- //
