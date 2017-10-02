2b538dc16235712f6f07f32f7f83ccbb5caed8dc
Jeremy Chone <jeremy.chone@gmail.com>
2017-10-01 18:42:27 -0700

    . remove the DsoRemote before commit (deploy) and use DsoMem

--- client/src/ts/init.ts
+++ client/src/ts/init.ts
@@ -14,11 +14,12 @@ import { get } from "./ajax"
 
 // --------- DataService Initialization --------- //
 // For the demo, we will have the Memory Dso fallback for any type the application might use. 	
-// ds.fallback(function (type) {
-// 	return new DsoMem(type);
-// });
+ds.fallback(function (type) {
+	return new DsoMem(type);
+});
 
-ds.register("Todo", new DsoRemote("Todo"));
+// This will use the file entity store with the crud API. All users same list for now. 
+// ds.register("Todo", new DsoRemote("Todo"));
 
 // For production, you might want to have some Entity DSO object that you would register as follow
 // ds.register("Task", new TaskDso());
